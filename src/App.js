773a7190d49e084c08dfa4e2e12f77138b5cdebb
losrobbos <losrobbos@web.de>
2020-05-26 17:08:02 +0200

    Added stage 2 solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Fruit from './Fruit';
 
 class App extends Component {
 
   constructor() {
     super()
 
-    this.state = { fruits: [] }
+    this.state = { 
+      fruits: [],
+      fruitNew: {title: '', color: ''}
+    }
   }
 
   // load our initial data
@@ -31,17 +35,56 @@ class App extends Component {
     this.setState({ fruits })
   }
 
+  handleFormTitleChange = (e) => {
+    this.setState({ fruitNew: { ...this.state.fruitNew, title: e.target.value}})
+  }
+
+  handleFormColorChange = (e) => {
+    this.setState({ fruitNew: { ...this.state.fruitNew, color: e.target.value}})
+  }
+
+  addFruit = (e) => {
+    e.preventDefault()
+    console.log(this.state.fruitNew)
+    let fruitsNew = [...this.state.fruits, this.state.fruitNew];
+    localStorage.setItem("FRUITS", JSON.stringify(fruitsNew))
+    this.setState({fruits: fruitsNew})
+  }
+
+  deleteFruit = (title) => {
+    let fruitsCopy = this.state.fruits.filter(fruit => fruit.title !== title)
+    localStorage.setItem("FRUITS", JSON.stringify(fruitsCopy))
+    this.setState({fruits: fruitsCopy})
+  }
+
+  changeColor = (title) => {
+    let fruitsCopy = [...this.state.fruits]
+    let fruitToUpdate = fruitsCopy.find(fruit => fruit.title == title)
+    fruitToUpdate.color = prompt("New color:", fruitToUpdate.color)
+    localStorage.setItem("FRUITS", JSON.stringify(fruitsCopy))
+    this.setState({fruits: fruitsCopy})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h2>Fruity lifecycle</h2>
         </header>
-        <main>    
-          <h4>Fruits</h4>
-          <div className="cards teachers">
+        <main>
+          <form onSubmit={this.addFruit}>
+            <input placeholder="Fruit..." onChange={this.handleFormTitleChange} value={this.state.fruitNew.title} />
+            <input placeholder="Color..." onChange={this.handleFormColorChange} value={this.state.fruitNew.color} />
+            <button type="submit">ADD</button>
+          </form>
+          {/* <h4>Fruits</h4> */}
+          <div className="cards fruits">
             {this.state.fruits.map(fruit => (
-              <div key={fruit.title}>{fruit.title}</div>
+              <Fruit 
+                fruit={fruit} 
+                changeColor={this.changeColor}
+                deleteFruit={this.deleteFruit}
+              ></Fruit>
             ))}
           </div>
         </main>
---
+++ src/Fruit.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Fruit = (props) => {
+  // fetch fruit from props
+  let { fruit, changeColor, deleteFruit } = props;
+  let fruitStyle = {
+    background: fruit.color
+  };
+
+  return (
+    <div className="fruit" style={fruitStyle} key={fruit.title} >
+      <span onClick={() => changeColor(fruit.title)}>{fruit.title}</span> 
+      <span onClick={() => deleteFruit(fruit.title)}>[X]</span>
+    </div>
+  );
+};
+
+export default Fruit;
